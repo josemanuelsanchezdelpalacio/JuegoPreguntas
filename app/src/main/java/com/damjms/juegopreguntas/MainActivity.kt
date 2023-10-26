a9ff2b1c460c9293cdca084af45795af75255dbe
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-10-26 12:37:09 +0200

    Juego preguntas con scaffold y hashmap

---
+++ app/src/main/java/com/damjms/juegopreguntas/MainActivity.kt
@@ -0,0 +1,271 @@
+package com.damjms.juegopreguntas
+
+import com.damjms.juegopreguntas.ui.theme.JuegoPreguntasTheme
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.damjms.juegopreguntas.data.listaPreguntas
+import com.damjms.juegopreguntas.data.preguntasRespuestas
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JuegoPreguntasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    preguntasAleatorias()
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+//metodo que recorre el hashMap y añade a la lista las preguntas. Despues las devuelve de forma aleatoria
+fun preguntasAleatorias(): String {
+    for (pregunta in preguntasRespuestas) {
+        listaPreguntas.add(pregunta.key)
+    }
+    return listaPreguntas.shuffle().toString()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting() {
+    var respuesta by rememberSaveable { mutableStateOf("") }
+    var numPartida by rememberSaveable { mutableStateOf(0) }
+    var puntuacion by rememberSaveable { mutableStateOf(0) }
+    val context = LocalContext.current
+
+    //creo un scaffold para poner un CenterAlignedTopAppBar donde poner la puntuacion y un titulo
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "TITULO DEL JUEGO",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                        Text(
+                            text = "PUNTUACION: $puntuacion",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color(0xFFFFC107),
+                    contentColorFor(Color.Black)
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.background),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = R.drawable.pelis_interrogante),
+                contentDescription = "interrogantePeliLogo",
+                alignment = Alignment.Center
+            )
+            //si el numero de la partida es menor al tamaño de la lista continua el juego
+            //cuando sea mayor saca un mensaje de FIN DEL JUEGO
+            if (numPartida < listaPreguntas.size) {
+                //muestro la pregunta a traves de la lista usando "numPartida" para sacar una de las preguntas
+                Text(
+                    text = listaPreguntas[numPartida],
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                OutlinedTextField(
+                    value = respuesta,
+                    onValueChange = { respuesta = it },
+                    label = { Text(text = "Introduce tu respuesta") },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                Button(
+                    onClick = {
+                        //si la respuesta coincide con el numero de la pregunta dentro de la lista saca mensaje de correcto, aumenta la puntuacion y elimina la pregunta ya mostrada.
+                        if (respuesta == preguntasRespuestas[listaPreguntas[numPartida]]) {
+                            Toast.makeText(context, "CORRECTO. Has acertado", Toast.LENGTH_SHORT)
+                                .show()
+                            puntuacion++
+                            preguntasRespuestas.remove(listaPreguntas[numPartida])
+                        } else {
+                            //si no es correcta muestro el mensaje de error con la respuesta correcta
+                            Toast.makeText(
+                                context,
+                                "ERROR. La respuesta correcta es: ${preguntasRespuestas[listaPreguntas[numPartida]]}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                        numPartida++
+                        respuesta = ""
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Text(text = "Validar")
+                }
+            } else {
+                Toast.makeText(context, "FIN DEL JUEGO", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+//todo lo comentado aqui seria para hacerlo a traves del metodo fun preguntasAleatorias() el cual directamente aleatoriza un HashMap
+/*
+fun preguntasAleatorias(): String {
+    return preguntasRespuestas.keys.random()
+}
+
+@Composable
+fun Greeting() {
+    var pregunta by rememberSaveable { mutableStateOf("") }
+    var respuesta by rememberSaveable { mutableStateOf("") }
+    var puntuacion by rememberSaveable { mutableStateOf(0) }
+    val context = LocalContext.current
+
+    Column {
+        Text(
+            text = "PUNTUACION: $puntuacion",
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.background),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = R.drawable.pelis_interrogante),
+                contentDescription = "interrogantePeliLogo",
+                alignment = Alignment.Center
+            )
+
+            Text(
+                text = pregunta,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            OutlinedTextField(
+                value = respuesta,
+                onValueChange = { respuesta = it },
+                label = { Text(text = "Introduce tu respuesta") },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Button(
+                onClick = {
+                    //si la respuesta coincide con la pregunta dentro del hashMap aumenta la puntuacion y vuelve a sacar una pregunta aleatoria
+                    if (respuesta == preguntasRespuestas[pregunta]) {
+                        Toast.makeText(
+                            context,
+                            "CORRECTO. Has acertado",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        puntuacion++
+                        preguntasRespuestas.remove(pregunta)
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "ERROR. La respuesta correcta es: ${preguntasRespuestas[pregunta]}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    pregunta = preguntasAleatorias()
+                    respuesta = ""
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = "Validar")
+            }
+        }
+    }
+}*/
---
+++ app/src/main/java/com/damjms/juegopreguntas/data/ListaPreguntasRespuestas.kt
@@ -0,0 +1,12 @@
+package com.damjms.juegopreguntas.data
+
+val preguntasRespuestas = mutableMapOf(
+    "¿Quién dirigió Reservoir Dogs?" to "Quentin Tarantino",
+    "¿Quién protagoniza El Pianista?" to "Adrien Brody",
+    "¿Cuántos Oscars consiguió Titanic?" to "11",
+    "¿Quién dirigió El Gran Lebowski?" to "Hermanos Coen",
+    "¿En qué año se estreno Matrix?" to "1999"
+)
+
+//lista para guardar las preguntas del hashMap
+val listaPreguntas = mutableListOf<String>()
